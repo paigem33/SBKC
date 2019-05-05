4215977a1b2cb8d70092a73eeb463de83918756b
paigem33 <paigemacgregor33@gmail.com>
2019-05-04 22:10:19 -0700

    updated nav

--- newSBKC/root/scripts/main.js
+++ newSBKC/root/scripts/main.js
@@ -11,10 +11,10 @@ $(function(){
         $('#sub2').stop().slideToggle();
         $('.fa-chevron-down:last').toggleClass('fa-chevron-up');
     })
-    $('.linkFake:first').hover(function(){
-        $('#sub1').stop().slideToggle();
-    })
-    $('.linkFake:last').hover(function(){
-        $('#sub2').stop().slideToggle();
-    })
+//    $('.linkFake:first').hover(function(){
+//        $('#sub1').stop().slideToggle();
+//    })
+//    $('.linkFake:last').hover(function(){
+//        $('#sub2').stop().slideToggle();
+//    })
 })
