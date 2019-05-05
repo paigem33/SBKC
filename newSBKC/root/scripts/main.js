cf75f03262e13506cdcaa52f94a390cc08484b72
paigem33 <paigemacgregor33@gmail.com>
2019-05-04 22:02:23 -0700

    converting to sass, changing layout

---
+++ newSBKC/root/scripts/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('i.hamMenu').click(function(){
+        $('nav').stop().slideToggle();
+        $('i.hamMenu').toggleClass('fa-times');
+    })
+    $('.linkFake:first').click(function(){
+        $('#sub1').stop().slideToggle();
+        $('.fa-chevron-down:first').toggleClass('fa-chevron-up');
+    })
+    $('.linkFake:last').click(function(){
+        $('#sub2').stop().slideToggle();
+        $('.fa-chevron-down:last').toggleClass('fa-chevron-up');
+    })
+    $('.linkFake:first').hover(function(){
+        $('#sub1').stop().slideToggle();
+    })
+    $('.linkFake:last').hover(function(){
+        $('#sub2').stop().slideToggle();
+    })
+})
