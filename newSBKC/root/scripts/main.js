3fef233afbf0da35ff26c8d3daf0edd60198bfef
paigem33 <paigemacgregor33@gmail.com>
2019-05-05 16:08:32 -0700

    finished index.html

--- newSBKC/root/scripts/main.js
+++ newSBKC/root/scripts/main.js
@@ -3,18 +3,18 @@ $(function(){
         $('nav').stop().slideToggle();
         $('i.hamMenu').toggleClass('fa-times');
     })
-    $('.linkFake:first').click(function(){
+    $('.disabled:first').click(function(){
         $('#sub1').stop().slideToggle();
         $('.fa-chevron-down:first').toggleClass('fa-chevron-up');
     })
-    $('.linkFake:last').click(function(){
+    $('.disabled:last').click(function(){
         $('#sub2').stop().slideToggle();
         $('.fa-chevron-down:last').toggleClass('fa-chevron-up');
     })
-//    $('.linkFake:first').hover(function(){
-//        $('#sub1').stop().slideToggle();
-//    })
-//    $('.linkFake:last').hover(function(){
-//        $('#sub2').stop().slideToggle();
-//    })
+    $('.disabled:first').hover(function(){
+        $('#sub1').stop().slideToggle();
+    })
+    $('.disabled:last').hover(function(){
+        $('#sub2').stop().slideToggle();
+    })
 })
